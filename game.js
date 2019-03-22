02bb73bd17088b4b03255eed9be98377db9d34e6
reiko <aoikomete@gmail.com>
2019-03-22 19:01:08 +0200

    UPD: Resizing the game to cover the wider area possible

--- game.js
+++ game.js
@@ -1,3 +1,29 @@
+var game;
 window.onload = function () {
-	var game = new Phaser.Game();
-};
+	var gameConfig = {
+		width:                  480,
+		height:                 640,
+		backgroundColor:        0xff0000
+	};
+	
+	game = new Phaser.Game(gameConfig);
+	window.focus();
+	resizeGame();
+	window.addEventListener("resize", resizeGame);
+}
+
+function resizeGame() {
+	var canvas                  = document.querySelector("canvas"),
+		windowWidth             = window.innerWidth,
+		windowHeight            = window.innerHeight,
+		windowRatio             = windowWidth / windowHeight,
+		gameRatio               = game.config.width / game.config.height;
+	
+	if(windowRatio < gameRatio) {
+		canvas.style.width = windowWidth + "px";
+		canvas.style.height = (windowWidth / gameRatio) + "px";
+	} else {
+		canvas.style.width = (windowHeight * gameRatio) + "px";
+		canvas.style.height = windowHeight + "px";
+	}
+}
