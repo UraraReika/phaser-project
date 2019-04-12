4758908cc90acdf0a552508b173e116690300643
reiko <aoikomete@gmail.com>
2019-04-12 09:47:36 +0300

    UPD / ADD: Move customizable variables in a single place / Create tile graphics as a sprite sheet and add it in the game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,16 @@
-var game;
+var game,
+	gameOptions = {
+		tileSize: 	 200,
+		tileSpacing: 20,
+		boardSize: 	 {
+			rows: 4,
+			cols: 4
+		}
+	}
 window.onload = function () {
 	var gameConfig = {
-		width:                  900,
-		height:                 900,
+		width:                  gameOptions.boardSize.cols * (gameOptions.tileSize + gameOptions.tileSpacing) + gameOptions.tileSpacing,
+		height:                 gameOptions.boardSize.rows * (gameOptions.tileSize + gameOptions.tileSpacing) + gameOptions.tileSpacing,
 		backgroundColor:        0xecf0f1,
 		scene: [ bootGame, playGame ]
 	};
@@ -19,6 +27,10 @@ class bootGame extends Phaser.Scene {
 	}
 	preload() {
 		this.load.image( "emptytile", "assets/images/emptytile.png" );
+		this.load.spritesheet( "tiles", "assets/images/tiles.png", {
+			frameWidth:  gameOptions.tileSize,
+			frameHeight: gameOptions.tileSize
+		} );
 	}
 	create() {
 		console.log( "Game is booting..." );
@@ -32,11 +44,19 @@ class playGame extends Phaser.Scene {
 	}
 	create() {
 		console.log( "This is my awesome game." );
-		for( var i = 0; i < 4; i++ ) {
-			for( var j = 0; j < 4; j++ ) {
-				this.add.image( 120 + j * 220, 120 + i * 220, "emptytile" );
+		for( var i = 0; i < gameOptions.boardSize.rows; i++ ) {
+			for( var j = 0; j < gameOptions.boardSize.cols; j++ ) {
+				var tilePosition = this.getTilePosition(i, j);
+				this.add.image( tilePosition.x, tilePosition.y, "emptytile" );
+				this.add.sprite( tilePosition.x, tilePosition.y, "tiles", 0 );
 			}
 		}
+	}
+	getTilePosition(row, col) {
+		var posX = gameOptions.tileSpacing * (col + 1) + gameOptions.tileSize * (col + 0.5),
+		    posY = gameOptions.tileSpacing * (row + 1) + gameOptions.tileSize * (row + 0.5);
+
+		return new Phaser.Geom.Point(posX, posY);
 	}
 }
 
